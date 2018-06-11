6790a40c442183248574cf8eb97aa04bea5d5559
dev <dev@devlap.devlap>
2018-06-10 22:03:32 -0400

    added shutdown functionality

--- multi_recieve.py
+++ multi_recieve.py
@@ -7,15 +7,6 @@ import time
 multicast_group = '239.0.0.1'
 server_address = ('', 5007)
 
-# def to convert bytes to ints for data from thermostat
-def bytes_to_int(bytes):
-    result = 0
-
-    for b in bytes:
-        result = result * 256 + int(b)
-
-    return result
-
 # Create the socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -31,16 +22,21 @@ sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 while True:
     print ('\nwaiting to receive message', file=sys.stderr)
     data, address = sock.recvfrom(1024)
-    
-    print ('received %s bytes from %s' % (len(data), address), file=sys.stderr)
-    print (data.decode(), file=sys.stderr)
-    
-    result = bytes_to_int(data)
+
+    print ('received from ', address, file=sys.stderr)
+
+    result = float(data.decode())
+
+    if result < 70:
+        print('\033[1;32mTemperature is %s \nRoom is still safe\033[1;m' % result)
 
     if result > 70:
-    	os.system('shutdown -h now')
-    break
-   
+        print('\033[1;31mTemperature is %s \nRoom is NOT still safe\033[1;m' % result)
+        print ('Shuting down in 5 seconds')
+        time.sleep(5)
+        os.system('shutdown -h now')
+        break
+ 
     print ('sending acknowledgement to', address, file=sys.stderr)
     sock.sendto('Recieved temperature Message'.encode(), address)
 
